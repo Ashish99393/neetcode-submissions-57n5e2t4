e4875152228ac1128b67e938381ba0c64d6cf9f3
Ashish Hembram <59952371+Ashish99393@users.noreply.github.com>
2026-04-24 21:33:36 +0530

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+        int n = strs.length;
+        String prefix = strs[0];
+        for (int i = 1; i < n; i++) {
+            int j = 0;
+            while (j < prefix.length() && j < strs[i].length()
+                && prefix.charAt(j) == strs[i].charAt(j)) {
+                j++;
+            }
+
+            prefix = prefix.substring(0, j);
+        }
+        return prefix;
+    }
+}
